436abe8d424f7dbe81cf1c4346073897ef3c7d3e
Maxim Kremenev <ezotrank@gmail.com>
2012-10-22 20:15:32 +0300

    add Mains controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Velican::Application.routes.draw do
   ActiveAdmin.routes(self)
 
   devise_for :admin_users, ActiveAdmin::Devise.config
+
+  resource :main, only: :show
+
+  root to: 'mains#index'
 end
