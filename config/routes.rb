eac2bbc6de50a4803d86beacf361f355755a69ec
Maxim Kremenev <ezotrank@gmail.com>
2012-10-25 10:20:23 +0300

    add nude controllers and templates

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Velican::Application.routes.draw do
 
   devise_for :admin_users, ActiveAdmin::Devise.config
 
+  resources :products, only: [:index, :show]
+  resource :cart, only: [:show]
   resource :main, only: :show
-
   root to: 'mains#index'
 end
