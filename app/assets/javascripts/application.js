349ff9dbd493192429de10b619f6fc42ce2021f0
Maxim Kremenev <ezotrank@gmail.com>
2012-10-25 14:46:36 +0300

    integrated makeup of home and products page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,33 @@
 //= require jquery
 //= require jquery_ujs
 //= require rails.validations
+
+$(document).ready(function(){
+   $('#main .popup a').click(function() {
+                $('#order').show();
+        });
+
+        $('#order .order_cl button').click(function() {
+                $('#order').hide();
+        });
+
+        $('#catalog_main .cart a').click(function() {
+                $('#order').show();
+        });
+
+        $('#order .order_cl button').click(function() {
+                $('#order').hide();
+        });
+
+        $('#catalog_main .item_container').click(function() {
+                $('#popup_pr').show();
+        });
+
+        $('#popup_pr .cart a').click(function() {
+                $('#order').show();
+        });
+
+        $('#popup_pr').click(function() {
+                $('#popup_pr').hide();
+        });
+});
