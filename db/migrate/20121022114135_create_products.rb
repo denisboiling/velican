8e6b8961cce7610332f8a2f227d91f0d838f3af2
Maxim Kremenev <ezotrank@gmail.com>
2012-10-29 16:31:30 +0300

    try fix decimal problem by load sample

--- db/migrate/20121022114135_create_products.rb
+++ db/migrate/20121022114135_create_products.rb
@@ -2,7 +2,7 @@ class CreateProducts < ActiveRecord::Migration
   def change
     create_table :products do |t|
       t.string :title, :size_range, null: false
-      t.decimal :price, :precision => 6, :scale => 4, null: false
+      t.decimal :price, :precision => 10, :scale => 2, null: false
       t.integer :label_id
       t.integer :category_id, null: false
       t.attachment :logo
--- db/migrate/20121026081825_create_line_items.rb
+++ db/migrate/20121026081825_create_line_items.rb
@@ -2,7 +2,7 @@ class CreateLineItems < ActiveRecord::Migration
   def change
     create_table :line_items do |t|
       t.integer :product_id, :order_id, :count, null: false
-      t.decimal :price, :precision => 6, :scale => 4, null: false, default: 0.00
+      t.decimal :price, :precision => 10, :scale => 2, null: false, default: 0.00
       t.timestamps
     end
   end
