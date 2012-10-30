0fce2b171d907e38176b06c7f32e5f9f87f8cb14
Maxim Kremenev <ezotrank@gmail.com>
2012-10-30 12:06:37 +0300

    add catalog menu

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,6 +5,8 @@ class ProductsController < ApplicationController
 		  Label.find_by_title(params[:label]).products.paginate(:page => params[:page], :per_page => 8)
                 elsif params[:q].present?
                   Product.search(title_contains: params[:q]).paginate(:page => params[:page], :per_page => 8)
+                elsif params[:category].present?
+                  Category.find_by_title(params[:category]).products.paginate(:page => params[:page], :per_page => 8)
 		else
 		  Product.paginate(:page => params[:page], :per_page => 8)
 		end
