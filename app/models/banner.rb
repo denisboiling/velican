4fc952a219b811c6eb25c5393a200bfd372bdd75
Maxim Kremenev <ezotrank@gmail.com>
2012-12-06 12:09:01 +0300

    fix banner size

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,5 +1,5 @@
 class Banner < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "975x362!", thumb: "125x125" },
+  has_attached_file :image, styles: { medium: "799x234>", thumb: "125x125" },
                             path: ":rails_root/public/system/:class/:attachment/:id/:style/:filename",
                             url: "/system/:class/:attachment/:id/:style/:filename",
                             default_style: :thumb
